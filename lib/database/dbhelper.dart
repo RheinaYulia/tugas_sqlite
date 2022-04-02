08be7eb12d81a414739874a07e38eeacffcb98cc
RheinaYulia <rheinayulia27@gmail.com>
2022-04-03 06:31:14 +0700

    kodeStok

--- lib/database/dbhelper.dart
+++ lib/database/dbhelper.dart
@@ -1,65 +1,90 @@
 import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:tugas_sqlite/item/item.dart';
 
 class DbHelper {
-  static final DbHelper _instance = DbHelper._internal();
+  static DbHelper? _dbHelper;
   static Database? _database;
 
-  final String tableName = 'tablePegawai';
-  final String columnId = 'id';
-  final String columnKode = 'kode';
-  final String columnName = 'name';
-  final String columnPrice = 'price';
-  final String columnStok = 'stok';
+  DbHelper._createObject();
+  Future<Database> initDb() async {
+    //untuk menentukan nama database dan lokasi yg dibuat
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'item1.db';
 
-  DbHelper._internal();
-  factory DbHelper() => _instance;
+    //create, read databases
+    var itemDatabase = openDatabase(path, version: 4, onCreate: _createDb);
 
-  Future<Database?> get _db async {
-    if (_database != null) {
-      return _database;
+    //mengembalikan nilai object sebagai hasil dari fungsinya
+    return itemDatabase;
+  }
+
+  //buat tabel baru dengan nama item
+  void _createDb(Database db, int version) async {
+    await db.execute('''
+ CREATE TABLE item (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ kode TEXT,
+ name TEXT,
+ price INTEGER,
+ stok INTEGER
+ )
+ ''');
+  }
+
+  //select databases
+  Future<List<Map<String, dynamic>>> select() async {
+    Database db = await this.initDb();
+    var mapList = await db.query('item', orderBy: 'name');
+    return mapList;
+  }
+
+//create databases
+  Future<int> insert(Item object) async {
+    Database db = await this.initDb();
+    int count = await db.insert('item', object.toMap());
+    return count;
+  }
+
+//update databases
+  Future<int> update(Item object) async {
+    Database db = await this.initDb();
+    int count = await db
+        .update('item', object.toMap(), where: 'id=?', whereArgs: [object.id]);
+    return count;
+  }
+
+//delete databases
+  Future<int> delete(int id) async {
+    Database db = await this.initDb();
+    int count = await db.delete('item', where: 'id=?', whereArgs: [id]);
+    return count;
+  }
+
+  Future<List<Item>> getItemList() async {
+    var itemMapList = await select();
+    int count = itemMapList.length;
+    List<Item> itemList = <Item>[];
+    for (int i = 0; i < count; i++) {
+      itemList.add(Item.fromMap(itemMapList[i]));
     }
-    _database = await _initDb();
-    return _database;
+    return itemList;
   }
 
-  Future<Database?> _initDb() async {
-    String databasePath = await getDatabasesPath();
-    String path = databasePath + 'ittem.db';
-
-    return await openDatabase(path, version: 4, onCreate: _createDb);
+  factory DbHelper() {
+    if (_dbHelper == null) {
+      _dbHelper = DbHelper._createObject();
+    }
+    return _dbHelper!;
   }
 
-  Future<void> _createDb(Database db, int version) async {
-    var sql = "CREATE TABLE $tableName($columnId INTEGER PRIMARY KEY, "
-        "$columnKode TEXT"
-        "$columnName TEXT,"
-        "$columnPrice INTEGER,"
-        "$columnStok INTEGER)";
-    await db.execute(sql);
-  }
-
-  Future<int?> insert(Item item) async {
-    var dbClient = await _db;
-    return await dbClient!.insert(tableName, item.toMap());
-  }
-
-  Future<List?> selectAll() async {
-    var dbClient = await _db;
-    var result = await dbClient!.query(tableName,
-        columns: [columnId, columnKode, columnName, columnPrice, columnStok]);
-    return result.toList();
-  }
-
-  Future<int?> update(Item item) async {
-    var dbClient = await _db;
-    return await dbClient!.update(tableName, item.toMap(),
-        where: '$columnId = ?', whereArgs: [item.id]);
-  }
-
-  Future<int?> delete(int id) async {
-    var dbClient = await _db;
-    return await dbClient!
-        .delete(tableName, where: '$columnId = ?', whereArgs: [id]);
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDb();
+    }
+    return _database!;
   }
 }
